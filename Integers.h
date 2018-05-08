2923611505c7edf511e646358e500a7bc120fc12
Cuddles04 <39076546+Cuddles04@users.noreply.github.com>
2018-05-08 14:25:51 +1000

    Space Wads

---
+++ Integers.h
@@ -0,0 +1,39 @@
+#pragma once
+//Integers and Bools ect.
+uint8_t gamestate = 0;
+uint8_t i = 0;
+uint8_t i2 = 0;
+//Player
+class Player
+{
+  public:
+    uint8_t x = 0;
+    uint8_t y = 0;
+    bool pause = false;
+    uint8_t lives = 3;
+};
+Player player = player;
+//Star                                                                                                                                                     
+ struct Star
+{
+  public:
+    uint8_t x = 0;
+    uint8_t y = 0;
+    bool active;
+    bool randomize = true;
+};
+  Star star[25];
+
+//Asteroid
+struct Asteroid
+{
+  public:
+    uint8_t x = 0;
+    uint8_t y = 0;
+    bool active;
+    bool randomize = true;
+};
+  Asteroid asteroid[10];
+  
+
+
