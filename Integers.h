08735599192f345b0d6163867122f26ef2b8b552
Filmote <simon@bloggingadeadhorse.com>
2018-05-08 21:19:33 +1000

    More stuff

--- Integers.h
+++ Integers.h
@@ -1,8 +1,18 @@
 #pragma once
+
+#define NUMBER_OF_STARS 25
+#define NUMBER_OF_ASTEROIDS 10
+#define NUMBER_OF_BULLETS 5
+
+#define MODE_ACTIVE 4
+#define MODE_EXPLOSION_3 3
+#define MODE_EXPLOSION_2 2
+#define MODE_EXPLOSION_1 1
+#define MODE_INACTIVE 0
 //Integers and Bools ect.
 uint8_t gamestate = 0;
-uint8_t i = 0;
-uint8_t i2 = 0;
+
+
 //Player
 class Player
 {
@@ -10,9 +20,10 @@ class Player
     uint8_t x = 0;
     uint8_t y = 0;
     bool pause = false;
+    uint8_t mode = 0;  // 0 = inactive, 1 = explosion1, 2 = explostion2, 3 = explosion3, 4 = active
     uint8_t lives = 3;
 };
-Player player = player;
+Player player;
 //Star                                                                                                                                                     
  struct Star
 {
@@ -23,7 +34,7 @@ Player player = player;
     bool active;
     bool randomize = true;
 };
-  Star star[25];
+Star star[NUMBER_OF_STARS];
 
 //Asteroid
 struct Asteroid
@@ -32,10 +43,16 @@ struct Asteroid
     uint8_t x = 0;
     uint8_t y = 0;
     uint8_t inc = 0;
-    bool active;
+    uint8_t mode = 0;  // 0 = inactive, 1 = explosion1, 2 = explostion2, 3 = explosion3, 4 = active
     bool randomize = true;
 };
-  Asteroid asteroid[10];
-  
 
+Asteroid asteroid[NUMBER_OF_ASTEROIDS];
+  
+struct Bullet {
+  uint8_t x = 0;
+  int8_t y = 0;
+};
+
+Bullet bullet[NUMBER_OF_BULLETS];
 
